a4a135492504c26bdc59dff80c8b7e9171b1ad96
Chris Thom <christopher.w.thom@outlook.com>
2019-01-11 19:58:14 +0000

    feat: begin work on webpacking js files

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+/*
+
+The entirety of this script is taken from the equivalent in the leaflet.extras
+package: https://github.com/bhaskarvk/leaflet.extras/blob/master/webpack.config.js
+
+Full credit to bhaskarvk for this script.
+
+*/
+
+const path = require('path');
+
+const binding_path = "./inst/htmlwidgets/bindings/";
+const build_path = path.resolve(__dirname, "./inst/htmlwidgets/build");
+
+let library_prod = function(name, filename = name) {
+  let foldername = filename;
+  filename = filename + "-prod"
+  var config = {
+	mode: "production", //production run, minifies the file
+	entry: name, //entry point is file passed to function
+	devtool: "source-map", //produce a sibling source map file
+	externals: {
+	  // make L available
+	  leaflet: "L"
+	},
+	output: {
+	  filename: filename + ".js",
+	  path: build_path + "/" + foldername
+	}
+  }
+  return config;
+}
+
+let library_binding = function(name) {
+  let filename = binding_path + name + "-bindings.js";
+  return {
+	mode: "production", // production run, minifies the file
+	entry: filename,
+	module: {
+	  // lint the bindings using ./inst/htmlwidgets/bindings/.eslintrc.js
+	  rules: [
+	    {
+		  test: /\.js$/,
+		  exclude: /node_modules/,
+		  load: "eslint-loader"
+	    },
+	  ]
+	},
+	output: {
+	  filename: name + "-bindings.js",
+	  path: build_path + "/" name
+	}
+  }
+}
+
+
+
+
+	
