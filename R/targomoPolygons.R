3e1ec4bbca337438aaedb73013de96ffe55b941b
Chris Mason-Thom <chrismasonthom@gmail.com>
2019-05-28 21:29:58 +0100

    feat: get httr request to REST api working

--- R/targomoPolygons.R
+++ R/targomoPolygons.R
@@ -1,44 +1,46 @@
-#' Add Targomo Polygons
+#' Targomo API base URL
 #'
-#' This function and the associated options allow you to easily add isochrones from
-#' the Targomo Polygon API to your leaflet map.
-#'
-#' @param map A leaflet map
-#' @param api_key Your Targomo API key - defaults to the \code{TARGOMO_API_KEY} environment variable.
-#' @param region Your Targomo region - defaults to the \code{TARGOMO_REGION} environment variable.
-#'   See here for the different regions: \url{https://targomo.com/developers/availability/}
-#' @param lat,lng Coordinates of the source of the polygons
-#' @param options See \code{\link{targomoOptions}}.
-#' @param layerId The leaflet map layerId for the resulting polygons.
-#' @param group The leaflet map group for the polygons.
-#' @param fitBounds Should the map rezoom to fit the bounds of the polygons?
-#'
-#' @name addTargomoPolygons
+targomo_api <- function() {
+  return("https://api.targomo.com/")
+}
 
+#' TODO: add function to create a source object, so that it can be mapped over
+#' the rows of the output of leaflet::derivePoints.
+
+#' Get Targomo Polygon GeoJSON data
+#'
+#' Function to call the Targomo API and return geojson.
+#'
+#' @param api_key
+#' @param region
+#' @param data
+#' @param lat,lng
+#' @param options
+#' @param ...
+#'
 #' @export
-addTargomoPolygons = function(map,
-                              api_key = Sys.getenv("TARGOMO_API_KEY"),
+getTargomoGeoJSON <- function(api_key = Sys.getenv("TARGOMO_API_KEY"),
                               region = Sys.getenv("TARGOMO_REGION"),
-                              lat = 55.9, lng = -3.1,
-                              layerId = NULL,
-                              group = NULL,
+                              data = NULL, lat = NULL, lng = NULL,
                               options = targomoOptions(),
-                              popup = NULL,
-                              popupOptions = NULL,
-                              label = NULL,
-                              labelOptions = NULL,
-                              data = leaflet::getMapData(map)) {
+                              ...) {
 
-  map$dependencies <- c(map$dependencies,
-                        targomoDependency())
+  url <- paste0(targomo_api(), region, "/v1/polygon?key=", api_key)
+  auth <- httr::add_headers(key = api_key)
 
-  points <- leaflet::derivePoints(data, lng = lng, lat = lat)
+  sources <- list("id" = seq_along(lat), "lat" = lat, "lng" = lng,
+                  "tm" = list("bike" = c()))
+  options$sources <- list(sources)
+  body <- jsonlite::toJSON(options, auto_unbox = TRUE, pretty = TRUE)
 
-  leaflet::invokeMethod(map, data, 'addTargomoPolygons',
-                        api_key, region, points, layerId, group, options,
-                        popup, popupOptions, label, labelOptions)
+  resp <- httr::POST(url = url, body = body, httr::verbose(), encode = "json",
+                     httr::add_headers("Content-Type" = "application/json"))
+
+  return(resp)
 
 }
+
+
 
 #' Set Targomo Options
 #'
@@ -47,8 +49,6 @@ addTargomoPolygons = function(map,
 #' @param travelTimes A vector of times, in seconds - each time corresponds to a
 #'     different polygon. Your API key will determine how many you can add.
 #' @param travelType What mode of transport to use - car, bike, walk or public transport.
-#' @param strokeWidth The weight of stroke used to draw the polygons.
-#' @param inverse Should the polygons be inverted?
 #' @param intersectionMode Whether to calculate the union or intersection of multiple sources
 #' @param ... Further arguments to pass to the API - see \url{https://docs.targomo.com/core/#/Polygon_Service/post_westcentraleurope_v1_polygon}
 #'
@@ -57,18 +57,21 @@ addTargomoPolygons = function(map,
 #' @export
 targomoOptions = function(travelTimes = c(600, 1200, 1800),
                           travelType = "bike",
-                          strokeWidth = 20,
-                          inverse = FALSE,
                           intersectionMode = "union",
+                          serializer = "geojson",
+                          srid = 4326,
                           ...) {
 
   leaflet::filterNULL(
     list(
-      traveTimes = travelTimes,
+      edgeWeight = "time",
+      polygon = list(
+        values = travelTimes,
+        intersectionMode = intersectionMode,
+        serializer = serializer
+      ),
       travelType = travelType,
-      strokeWidth = strokeWidth,
-      inverse = inverse,
-      intersectionMode = intersectionMode,
+      srid = srid,
       ...
     )
   )
